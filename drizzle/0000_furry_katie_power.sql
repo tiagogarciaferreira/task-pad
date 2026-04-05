482c3acb6021e5c86240a1c16731bdf11d7a2e20
tiagogarciaferreira <tiagogarciaferreira@gmail.com>
2026-04-05 09:52:10 -0300

    feat: add `tb_tasks` table with schema for task management in Drizzle ORM
    - Introduced `tb_tasks` table to manage tasks with fields for title, description, status, priority, due date, tags, and timestamps.
    - Added default values and constraints for better data consistency and integrity.
    - Updated Drizzle migration snapshot and journal for version tracking.

---
+++ drizzle/0000_furry_katie_power.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "tb_tasks" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"title" text NOT NULL,
+	"description" text NOT NULL,
+	"estimatedHours" real NOT NULL,
+	"tags" text[] DEFAULT '{}' NOT NULL,
+	"status" text DEFAULT 'To Do' NOT NULL,
+	"userId" text NOT NULL,
+	"priority" text NOT NULL,
+	"dueDate" timestamp with time zone NOT NULL,
+	"createdAt" timestamp with time zone DEFAULT now() NOT NULL,
+	"updatedAt" timestamp with time zone DEFAULT now() NOT NULL
+);
